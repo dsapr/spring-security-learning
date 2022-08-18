dc4551c3b16dcc0d52b17d50e65ed767fa33c9f2
dsapr <785722840@qq.com>
2022-08-18 14:30:32 +0800

    first commit

---
+++ src/main/java/com/dsapr/springsecuritylearning/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.dsapr.springsecuritylearning.config;
+
+/**
+ * 安全配置
+ *
+ * @author: dsapr
+ * @since 1.0.0
+ */
+public class SecurityConfig {
+}
---
+++ src/test/java/com/dsapr/springsecuritylearning/SpringSecurityLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsapr.springsecuritylearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
